2bf48413f5faa0ccab31086258ac28105858bd00
pranay <partekepranay@gmail.com>
2022-01-18 21:28:09 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/Class1.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+	
+	@Test
+	public void method1() {
+		System.out.println("Method1 is execute");
+	}
+	
+	@Test
+	public void method2() {
+		System.out.println("Method2 is execute");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/Class2.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+
+	@Test
+	public void method3() {
+		System.out.println("Method3 is execute");
+	}
+	
+	@Test
+	public void method4() {
+		System.out.println("Method4 is execute");
+	}
+
+}
+
